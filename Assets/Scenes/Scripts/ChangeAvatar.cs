81c0bf376ed99eec7aee3d1be3da3b86cb23b93f
meh-who <62934421+meh-who@users.noreply.github.com>
2023-03-14 19:46:55 -0400

    add magic puff

--- Assets/Scenes/Scripts/ChangeAvatar.cs
+++ Assets/Scenes/Scripts/ChangeAvatar.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Normal.Realtime;
 
 public class ChangeAvatar : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Reference to Realtime to use to instantiate brush strokes
+    [SerializeField] private Realtime _realtime;
+
+    [SerializeField] private GameObject _magicParticles;
+    [SerializeField] private Transform _camera;
+    public Vector3 camPos => _camera.position;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void magicSpawn()
+    {
+        Realtime.Instantiate(_magicParticles.name, new Vector3(camPos.x, camPos.y, camPos.z), transform.rotation, new Realtime.InstantiateOptions
+        {
+            ownedByClient = true,
+            preventOwnershipTakeover = true,
+            destroyWhenOwnerLeaves = true,
+            destroyWhenLastClientLeaves = true,
+            useInstance = _realtime,
+        });
+    }
+
+
 }
